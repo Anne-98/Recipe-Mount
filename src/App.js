e6364085e49b3aaa6f689cc6068a9710b371bfff
Anne Sudari <anne.sudari@gmail.com>
2021-11-03 12:35:53 +0530

    exact true route get into bottom

--- src/App.js
+++ src/App.js
@@ -20,13 +20,13 @@ export default function App() {
       <NavbarFixed/>
       <Switch>
         <Route path="/Recipe-Mount/categories" component={CategoriesPage}/>
-        <Route path="/Recipe-Mount" exact={true} component={HomePage}/>
-        {/* <Route path="/" exact={true} component={HomePage}/> */}
         <Route path="/Recipe-Mount/:dynamic/:dynamic/:dynamic" render={(props) => {return(<SearchItemList {...props}/>)}}/>
         <Route path="/Recipe-Mount/:dynamic/:dynamic" render={(props) =>{return(<DetailsPage  {...props } /> )}} />
         <Route path="/Recipe-Mount/random" component={RandomPage}/>
         <Route path="/Recipe-Mount/:dynamic" render={(props) => { return ( <DynamicCategoriesPage {...props } /> )}}/>
         <Route path="/Recipe-Mount/search" component={SearchPage} />
+        <Route path="/Recipe-Mount" component={HomePage}/>
+        <Route path="/" exact={true} component={HomePage}/>
       </Switch>
       <Footer />
     </BrowserRouter>
